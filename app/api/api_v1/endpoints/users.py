ed62f641a0096bfe9c8c28319c3222a6dd382b65
dpetkov <d.petkov@grandenergy.net>
2021-11-03 22:22:11 +0200

    test

--- app/api/api_v1/endpoints/users.py
+++ app/api/api_v1/endpoints/users.py
@@ -26,6 +26,19 @@ def read_users(
     users = crud.user.get_multi(db, skip=skip, limit=limit)
     return users
 
+@router.get("/test", response_model=List[schemas.User])
+def test(
+    db: Session = Depends(deps.get_db),
+    skip: int = 0,
+    limit: int = 100,
+    current_user: models.User = Depends(deps.get_current_active_superuser),
+) -> Any:
+    """
+    Retrieve users.
+    """
+    print('AAAAAAAAAAAAA')
+    users = crud.user.get_multi(db, skip=skip, limit=limit)
+    return {'message':'maliii'}
 
 @router.post("/", response_model=schemas.User)
 def create_user(
