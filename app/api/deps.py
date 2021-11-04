381551d93a480a29565d7e66f9e657cfe26d7ee8
MorskiBG <dymityrp@yahoo.com>
2021-11-04 21:46:36 +0200

    jwt exception changed

--- app/api/deps.py
+++ app/api/deps.py
@@ -33,8 +33,9 @@ def get_current_user(
             token, settings.SECRET_KEY, algorithms=[security.ALGORITHM]
         )
         token_data = schemas.TokenPayload(**payload)
-    except (jwt.JWTError, ValidationError):
-        print(jwt.JWTError)
+    except jwt.exceptions.InvalidTokenError as e:
+
+        print(e)
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
             detail="Could not validate credentials",
