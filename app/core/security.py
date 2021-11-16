6976c9543227407a81a9cda8bbd664c69af765b2
MorskiBG <dymityrp@yahoo.com>
2021-11-16 17:02:48 +0200

    adding is_admin to token

--- app/core/security.py
+++ app/core/security.py
@@ -14,7 +14,7 @@ ALGORITHM = "HS256"
 
 
 def create_access_token(
-    subject: Union[str, Any], expires_delta: timedelta = None
+    subject: Union[str, Any], is_admin: bool = False, expires_delta: timedelta = None
 ) -> str:
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
@@ -22,7 +22,7 @@ def create_access_token(
         expire = datetime.utcnow() + timedelta(
             minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
         )
-    to_encode = {"exp": expire, "sub": str(subject)}
+    to_encode = {"exp": expire, "sub": str(subject), "is_admin": is_admin}
     encoded_jwt = jwt.encode(
         to_encode, settings.SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
