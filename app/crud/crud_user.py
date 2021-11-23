3926fe25aad2a3b93d1eb00931687737ce7ddb5d
MorskiBG <dymityrp@yahoo.com>
2021-11-24 00:28:27 +0200

    update 6

--- app/crud/crud_user.py
+++ app/crud/crud_user.py
@@ -38,7 +38,8 @@ class CRUDUser(CRUDBase[User, UserCreate, UserUpdate]):
             update_data = obj_in
         else:
             update_data = obj_in.dict(exclude_unset=True)
-        if update_data.get["password"]:
+            print(f'Update data ---> {update_data}')
+        if update_data["password"]:
             hashed_password = get_password_hash(update_data["password"])
             del update_data["password"]
             update_data["hashed_password"] = hashed_password
