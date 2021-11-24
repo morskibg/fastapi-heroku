bb275d6d8755c64b3f591a0250cded15f4ddc453
MorskiBG <dymityrp@yahoo.com>
2021-11-24 17:52:52 +0200

    wwewe

--- app/schemas/contractor.py
+++ app/schemas/contractor.py
@@ -7,7 +7,6 @@ from ..models import Address
 # Shared properties
 class ContractorBase(BaseModel):
     name: str
-    address_id: int
     eik: str
 
 
@@ -24,18 +23,17 @@ class ContractorUpdate(ContractorBase):
 # Properties shared by models stored in DB
 class ContractorInDBBase(ContractorBase):
     id: int
-
-    class Config:
-        orm_mode = True
-
-
-# Properties to return to client
-class Contractor(ContractorInDBBase):
     address: Address
 
     class Config:
         orm_mode = True
         arbitrary_types_allowed = True
+
+
+# Properties to return to client
+class Contractor(ContractorInDBBase):
+
+    pass
 
 
 # Properties properties stored in DB
