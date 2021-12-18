622c812149ce23deff5b6099aa521ec906ec4430
MorskiBG <dymityrp@yahoo.com>
2021-12-18 15:35:10 +0200

    permission for usual user to get stp codes

--- app/api/api_v1/endpoints/utils.py
+++ app/api/api_v1/endpoints/utils.py
@@ -13,7 +13,12 @@ from app import models, schemas
 from app.api import deps
 from app.core.celery_app import celery_app
 from app.core.config import settings
-from app.core.utils import convert_date_to_utc_with_hours, convert_date_from_utc, Montel_Reader, Spot
+from app.core.utils import (
+    convert_date_to_utc_with_hours,
+    convert_date_from_utc,
+    Montel_Reader,
+    Spot,
+)
 from app.utils import send_test_email
 
 router = APIRouter()
@@ -42,6 +47,7 @@ def test_email(
     send_test_email(email_to=email_to)
     return {"msg": "Test email sent"}
 
+
 # @router.get("/schedule", response_model=schemas.Msg, status_code=201)
 # def unique_itn_schedule(
 #     startDate: str,
@@ -55,42 +61,47 @@ def test_email(
 #     return {"msg": "Test email sent"}
 
 
-@router.get('/stp/{code}')
+@router.get("/stp/{code}")
 def excel(
     code: str,
-    start_date: str = '01/12/2021',
-    end_date: str = '31/12/2021',
+    start_date: str = "01/12/2021",
+    end_date: str = "31/12/2021",
     # type: str = 'raw',
-    current_user: models.User = Depends(deps.get_current_active_superuser),
+    current_user: models.User = Depends(deps.get_current_active_user),
 ) -> Any:
     # path = os.path.join(os.path.join(
     #                 app.root_path, app.config[curr_path]), file.filename)
     try:
-        start_date_obj = dt.datetime.strptime(start_date, '%d/%m/%Y')
-        end_date_obj = dt.datetime.strptime(end_date, '%d/%m/%Y')
+        start_date_obj = dt.datetime.strptime(start_date, "%d/%m/%Y")
+        end_date_obj = dt.datetime.strptime(end_date, "%d/%m/%Y")
         end_date_obj = end_date_obj.replace(hour=23)
 
-        start_date_utc = convert_date_to_utc_with_hours(
-            'Europe/Sofia', start_date_obj)
-        end_date_utc = convert_date_to_utc_with_hours(
-            'Europe/Sofia', end_date_obj)
+        start_date_utc = convert_date_to_utc_with_hours("Europe/Sofia", start_date_obj)
+        end_date_utc = convert_date_to_utc_with_hours("Europe/Sofia", end_date_obj)
         # print("🚀 ~ file: utils.py ~ line 60 ~ end_date_utc", end_date_utc)
 
         stp_codes = settings.STP_CODES
         df = pd.read_excel(
-            os.path.join(os.getcwd(), 'stp2021_parts.xlsx'), sheet_name=code.split('_')[0], index_col=0)
+            os.path.join(os.getcwd(), "stp2021_parts.xlsx"),
+            sheet_name=code.split("_")[0],
+            index_col=0,
+        )
         # print(df)
 
-        raw_df = df[(df['code'] == code) & (df.index >= start_date_utc)
-                    & (df.index <= end_date_utc)]
+        raw_df = df[
+            (df["code"] == code)
+            & (df.index >= start_date_utc)
+            & (df.index <= end_date_utc)
+        ]
         # print("🚀 ~ file: utils.py ~ line 84 ~ raw_df", raw_df)
 
         raw_df.reset_index(inplace=True)
 
-        raw_df = raw_df[['utc', 'value']]
-        raw_df['eet'] = raw_df['utc'].apply(
-            lambda x: str(convert_date_from_utc('EET', x, False)))
-        raw_df['utc'] = raw_df['utc'].astype(str, copy=False)
+        raw_df = raw_df[["utc", "value"]]
+        raw_df["eet"] = raw_df["utc"].apply(
+            lambda x: str(convert_date_from_utc("EET", x, False))
+        )
+        raw_df["utc"] = raw_df["utc"].astype(str, copy=False)
 
         if raw_df.empty:
             raise HTTPException(
@@ -100,32 +111,40 @@ def excel(
 
         # Montly
         month_start_date = start_date_obj.replace(day=1)
-        month_end_date = start_date_obj.replace(day=calendar.monthrange(
-            start_date_obj.year, month=int(start_date_obj.month))[1])
+        month_end_date = start_date_obj.replace(
+            day=calendar.monthrange(
+                start_date_obj.year, month=int(start_date_obj.month)
+            )[1]
+        )
         month_start_date_utc = convert_date_to_utc_with_hours(
-            'Europe/Sofia', month_start_date)
+            "Europe/Sofia", month_start_date
+        )
         month_end_date_utc = convert_date_to_utc_with_hours(
-            'Europe/Sofia', month_end_date)
-        monthly_df = df[(df['code'] == code) & (df.index >= month_start_date_utc)
-                        & (df.index <= month_end_date_utc)]
+            "Europe/Sofia", month_end_date
+        )
+        monthly_df = df[
+            (df["code"] == code)
+            & (df.index >= month_start_date_utc)
+            & (df.index <= month_end_date_utc)
+        ]
         monthly_df.reset_index(inplace=True)
 
-        monthly_df = monthly_df[['utc', 'value']]
-        monthly_df['eet'] = monthly_df['utc'].apply(
-            lambda x: convert_date_from_utc('EET', x, False))
-        monthly_df['utc'] = monthly_df['utc'].astype(str, copy=False)
-        monthly_df['week_day'] = monthly_df['eet'].dt.dayofweek
+        monthly_df = monthly_df[["utc", "value"]]
+        monthly_df["eet"] = monthly_df["utc"].apply(
+            lambda x: convert_date_from_utc("EET", x, False)
+        )
+        monthly_df["utc"] = monthly_df["utc"].astype(str, copy=False)
+        monthly_df["week_day"] = monthly_df["eet"].dt.dayofweek
 
-        monthly_df['is_work'] = monthly_df['week_day'].apply(lambda x: x < 5)
-        monthly_df['hour'] = monthly_df['eet'].dt.hour + 1
-        work_df = monthly_df[monthly_df['is_work']].groupby(['hour']).mean()
+        monthly_df["is_work"] = monthly_df["week_day"].apply(lambda x: x < 5)
+        monthly_df["hour"] = monthly_df["eet"].dt.hour + 1
+        work_df = monthly_df[monthly_df["is_work"]].groupby(["hour"]).mean()
         work_df.reset_index(inplace=True)
-        work_df = work_df[['hour', 'value']]
+        work_df = work_df[["hour", "value"]]
 
-        weekend_df = monthly_df[~monthly_df['is_work']].groupby([
-                                                                'hour']).mean()
+        weekend_df = monthly_df[~monthly_df["is_work"]].groupby(["hour"]).mean()
         weekend_df.reset_index(inplace=True)
-        weekend_df = weekend_df[['hour', 'value']]
+        weekend_df = weekend_df[["hour", "value"]]
         # print(f'work DF\n', work_df)
         # print(f'weekend DF\n', weekend_df)
 
@@ -134,10 +153,19 @@ def excel(
             status_code=404,
             detail="Wrong query params.",
         )
-    return [{code: raw_df.to_json(orient='index')},
-            {f'{calendar.month_name[start_date_obj.month]}-{start_date_obj.year}': work_df.to_json(
-                orient='index')},
-            {f'{calendar.month_name[start_date_obj.month]}-{start_date_obj.year}-WeekEnd': weekend_df.to_json(orient='index')}]
+    return [
+        {code: raw_df.to_json(orient="index")},
+        {
+            f"{calendar.month_name[start_date_obj.month]}-{start_date_obj.year}": work_df.to_json(
+                orient="index"
+            )
+        },
+        {
+            f"{calendar.month_name[start_date_obj.month]}-{start_date_obj.year}-WeekEnd": weekend_df.to_json(
+                orient="index"
+            )
+        },
+    ]
 
     # data_to_return = [{code: raw_df}, {
     #     calendar.month_name[start_date_obj.month]: work_df}]
@@ -146,11 +174,11 @@ def excel(
     # return {code: data_to_return.to_json(orient='index')}
 
 
-@router.get('/spots')
+@router.get("/spots")
 def spots(
     # code: str,
-    start_date: str = '01/12/2021',
-    end_date: str = '31/12/2021',
+    start_date: str = "01/12/2021",
+    end_date: str = "31/12/2021",
     # type: str = 'raw',
     # current_user: models.User = Depends(deps.get_current_active_superuser),
 ) -> Any:
@@ -159,15 +187,15 @@ def spots(
         #
         scraper = Spot(reader, start_date, end_date)
         data_df = scraper.get_data()
-        data_df['eet'] = data_df['utc'].apply(
-            lambda x: str(convert_date_from_utc('EET', x, False)))
+        data_df["eet"] = data_df["utc"].apply(
+            lambda x: str(convert_date_from_utc("EET", x, False))
+        )
         print("🚀 ~ file: utils.py ~ line 162 ~ data_df", data_df)
-        return [{'data': data_df.to_json(orient='index')}]
+        return [{"data": data_df.to_json(orient="index")}]
         # scraper.update_db(data)
 
     except Exception as e:
         print("🚀 ~ file: utils.py ~ line 165 ~ e", e)
         exc_type, exc_obj, exc_tb = sys.exc_info()
         print(exc_type, exc_obj, exc_tb.tb_lineno)
-        raise HTTPException(
-            status_code=500, detail="Server error !Spots not loaded !")
+        raise HTTPException(status_code=500, detail="Server error !Spots not loaded !")
--- app/core/config.py
+++ app/core/config.py
@@ -6,20 +6,29 @@ from pydantic import AnyHttpUrl, BaseSettings, EmailStr, HttpUrl, PostgresDsn, v
 
 class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
-    SECRET_KEY: str = 'g!F$Aa#)'
+    SECRET_KEY: str = "g!F$Aa#)"
     # SECRET_KEY: str = secrets.token_urlsafe(32)
     # 60 minutes * 24 hours * 8 days = 8 days
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
     # ACCESS_TOKEN_EXPIRE_MINUTES: int = 1
-    SERVER_NAME: str = 'https://softuni-react-backend.herokuapp.com/'
+    SERVER_NAME: str = "https://softuni-react-backend.herokuapp.com/"
     # https://backend-react-powerapp.herokuapp.com/
-    SERVER_HOST: AnyHttpUrl = 'http://localhost:8080'
+    SERVER_HOST: AnyHttpUrl = "http://localhost:8080"
     # BACKEND_CORS_ORIGINS is a JSON-formatted list of origins
     # e.g: '["http://localhost", "http://localhost:4200", "http://localhost:3000", \
     # "http://localhost:8080", "http://local.dockertoolbox.tiangolo.com"]'
-    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = ["http://localhost", "http://localhost:4200", "http://localhost:3000",
-                                              "http://localhost:8080", "http://local.dockertoolbox.tiangolo.com", "http://10.10.10.71:3000", "https://softuni-react-powerapp.herokuapp.com", "https://softuni-react-backend.herokuapp.com",  "https://softuni-react-powerapp-morskibg.vercel.app", "https://softuni-react-powerapp.vercel.app"
-                                              ]
+    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = [
+        "http://localhost",
+        "http://localhost:4200",
+        "http://localhost:3000",
+        "http://localhost:8080",
+        "http://local.dockertoolbox.tiangolo.com",
+        "http://10.10.10.71:3000",
+        "https://softuni-react-powerapp.herokuapp.com",
+        "https://softuni-react-backend.herokuapp.com",
+        "https://softuni-react-powerapp-morskibg.vercel.app",
+        "https://softuni-react-powerapp.vercel.app",
+    ]
 
     @validator("BACKEND_CORS_ORIGINS", pre=True)
     def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
@@ -29,7 +38,7 @@ class Settings(BaseSettings):
             return v
         raise ValueError(v)
 
-    PROJECT_NAME: str = 'softuni-backend'
+    PROJECT_NAME: str = "softuni-backend"
     # SENTRY_DSN: Optional[HttpUrl] = None
 
     # @validator("SENTRY_DSN", pre=True)
@@ -42,7 +51,9 @@ class Settings(BaseSettings):
     # POSTGRES_USER: str
     # POSTGRES_PASSWORD: str
     # POSTGRES_DB: str
-    SQLALCHEMY_DATABASE_URI: Optional[PostgresDsn] = "postgresql://ntxryrmkaudgpt:ecc84daa6ef96bf4041adfee079f1a0ca7317704b570efe0bcdcadf5e153e4bd@ec2-54-217-195-234.eu-west-1.compute.amazonaws.com:5432/detism4ur5phed"
+    SQLALCHEMY_DATABASE_URI: Optional[
+        PostgresDsn
+    ] = "postgresql://ntxryrmkaudgpt:ecc84daa6ef96bf4041adfee079f1a0ca7317704b570efe0bcdcadf5e153e4bd@ec2-54-217-195-234.eu-west-1.compute.amazonaws.com:5432/detism4ur5phed"
 
     # @validator("SQLALCHEMY_DATABASE_URI", pre=True)
     # def assemble_db_connection(cls, v: Optional[str], values: Dict[str, Any]) -> Any:
@@ -81,14 +92,38 @@ class Settings(BaseSettings):
     #         and values.get("SMTP_PORT")
     #         and values.get("EMAILS_FROM_EMAIL")
     #     )
-    EQ_API_KEY: str = 'ab5e81-7adacb-92a6ae-2185c9'
+    EQ_API_KEY: str = "ab5e81-7adacb-92a6ae-2185c9"
     EMAIL_TEST_USER: EmailStr = "dimityrp@yahoo.com"  # type: ignore
     FIRST_SUPERUSER: EmailStr = "dimityrp@yahoo.com"
-    FIRST_SUPERUSER_PASSWORD: str = '12345'
+    FIRST_SUPERUSER_PASSWORD: str = "12345"
     USERS_OPEN_REGISTRATION: bool = True
     EMAIL_GUEST_USER: EmailStr = "guestUser@demo.com"
-    STP_CODES: list = ['EPRO_B01', 'EPRO_B02', 'EPRO_B03', 'EPRO_B04',           'EPRO_H01', 'EPRO_H02', 'EPRO_S01', 'EVN_BD000', 'EVN_G0', 'EVN_G1',
-                       'EVN_G2', 'EVN_G3', 'EVN_G4', 'EVN_H0', 'EVN_H1', 'EVN_H2', 'CEZ_B1', 'CEZ_B2', 'CEZ_B3', 'CEZ_B4', 'CEZ_B5', 'CEZ_H1', 'CEZ_H2', 'CEZ_S1']
+    STP_CODES: list = [
+        "EPRO_B01",
+        "EPRO_B02",
+        "EPRO_B03",
+        "EPRO_B04",
+        "EPRO_H01",
+        "EPRO_H02",
+        "EPRO_S01",
+        "EVN_BD000",
+        "EVN_G0",
+        "EVN_G1",
+        "EVN_G2",
+        "EVN_G3",
+        "EVN_G4",
+        "EVN_H0",
+        "EVN_H1",
+        "EVN_H2",
+        "CEZ_B1",
+        "CEZ_B2",
+        "CEZ_B3",
+        "CEZ_B4",
+        "CEZ_B5",
+        "CEZ_H1",
+        "CEZ_H2",
+        "CEZ_S1",
+    ]
 
     class Config:
         case_sensitive = True
